3d23f9ee6e1f66abc9c4dd25d8c9ff028b1da6eb
robdir <rhwickham@gmail.com>
2018-02-15 14:38:09 +0100

    begin integration testing

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -1,24 +1,16 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
+import { run } from '@ember/runloop'
 
 moduleForComponent('rental-listing', 'Integration | Component | rental listing', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('it should display rental details', function(assert) {
 
-  this.render(hbs`{{rental-listing}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#rental-listing}}
-      template block text
-    {{/rental-listing}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
+
+test('should toggle wide class on click', function(assert) {
+  
+})
