f70dd7ac082671c8688271e4ff620906542059a9
robdir <rhwickham@gmail.com>
2018-02-15 14:41:02 +0100

    create test obj

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -3,14 +3,23 @@ import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
 import { run } from '@ember/runloop'
 
+let rental = EmberObject.create({
+  image: 'fake.png',
+  title: 'test-title',
+  owner: 'test-owner',
+  category: 'test-type',
+  city: 'test-city',
+  bedrooms: 3
+})
+
 moduleForComponent('rental-listing', 'Integration | Component | rental listing', {
   integration: true
 });
 
 test('it should display rental details', function(assert) {
-
+  this.set('rentalObj', rental);
 });
 
 test('should toggle wide class on click', function(assert) {
-  
+  this.set('rentalObj', rental);
 })
