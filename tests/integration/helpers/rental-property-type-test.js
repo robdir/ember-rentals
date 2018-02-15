ec8d63b2d73884c0614d6e2db972b85eb9e05aa2
robdir <rhwickham@gmail.com>
2018-02-15 15:16:03 +0100

    ember data management

--- tests/integration/helpers/rental-property-type-test.js
+++ tests/integration/helpers/rental-property-type-test.js
@@ -6,10 +6,18 @@ moduleForComponent('rental-property-type', 'helper:rental-property-type', {
 });
 
 // Replace this with your real tests.
-test('it renders', function(assert) {
-  this.set('inputValue', '1234');
+  test('it renders correctly for a Standalone rental', function (assert) {
+    this.set('inputValue', 'Estate');
 
-  this.render(hbs`{{rental-property-type inputValue}}`);
+    this.render(hbs`{{rental-property-type inputValue}}`);
 
-  assert.equal(this.$().text().trim(), '1234');
-});
+    assert.equal(this.$().text().trim(), 'Standalone');
+  });
+
+  test('it renders correctly for a Community rental', function (assert) {
+    this.set('inputValue', 'Apartment');
+
+    this.render(hbs`{{rental-property-type inputValue}}`);
+
+    assert.equal(this.$().text().trim(), 'Community');
+  })
